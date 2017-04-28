a8e03c7b98befa797b65282783f679b5d9c6ab88
pablpaez <pablopaez.velasco@gmail.com>
2017-04-28 13:11:14 +0200

    Delete StudentDao.java

--- DAO/src/StudentDao.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.List;
-
-public interface StudentDao {
-   public List<StudentTransfer> getAllStudents();
-   public StudentTransfer getStudent(int id);
-   public void updateStudent(StudentTransfer student);
-   public void deleteStudent(StudentTransfer student);
-}
