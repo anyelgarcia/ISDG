b4767e7144b069fd9c7ee6f1781f690598f04d58
Galieve <enriqueroman@live.com>
2017-05-12 14:03:29 +0200

    Add files via upload
    Que bonita es la vida, que bonito es el amor, que bonito es el atun.

--- DIedrAl/src/DIedrAl_Project/presentacion/Actividades.java
+++ DIedrAl/src/DIedrAl_Project/presentacion/Actividades.java
@@ -7,9 +7,7 @@ import java.awt.Insets;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-
 
 public class Actividades extends ImagePanel{
 	public Actividades(String img){
@@ -28,50 +26,15 @@ public class Actividades extends ImagePanel{
 		c.gridx = 0;
 		c.gridy = 1;
 		c.insets = new Insets(0, 0, 0, 0);
+		add(new JButton("Crear Nuevo"), c);
 		
-		
-		
-		JButton nuevo = new JButton("Crear Nuevo");
-		nuevo.addActionListener((ae) -> {
-			JFrame panel = new JFrame();
-			
-			panel.setSize(300, 400);
-			panel.setVisible(true);
-			panel.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			
-		});
-		add(nuevo, c);
-		
-		
-		
-		JButton buscar = new JButton("Buscar");
-		buscar.addActionListener((ae) -> {
-			JFrame panel = new JFrame();
-			
-			panel.setSize(300, 400);
-			panel.setVisible(true);
-			panel.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			
-		});
 		c.gridx = 1;
 		c.gridy = 1;
-		add(buscar, c);
+		add(new JButton("Buscar"), c);
 		
-		
-		
-		
-		JButton eliminar = new JButton("Eliminar");
-		eliminar.addActionListener((ae) -> {
-			JFrame panel = new JFrame();
-			
-			panel.setSize(300, 400);
-			panel.setVisible(true);
-			panel.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			
-		});
 		c.gridx = 2;
 		c.gridy = 1;
-		add(eliminar, c);
+		add(new JButton("Eliminar"), c);
 		setVisible(true);
 	}
 }
--- DIedrAl/src/DIedrAl_Project/presentacion/Main.java
+++ DIedrAl/src/DIedrAl_Project/presentacion/Main.java
@@ -1,21 +1,20 @@
 package DIedrAl_Project.presentacion;
 
 import java.awt.GridLayout;
-import java.awt.event.WindowEvent;
 
 import javax.swing.*;
 
 public class Main {
 	
-	public static void run(){
-		JFrame pantalla = new JFrame();
+	public static void run(JFrame pantalla){
+		pantalla = new JFrame();
 		pantalla.setLayout(new GridLayout(3, 3, 1, 1));
 		pantalla.add(new Calendario("images/diedral00.png"));
-		pantalla.add(new Pacientes("images/diedral00.png"));
-		pantalla.add(new Perfil("images/diedral00.png"));
+		pantalla.add(new Pacientes());
+		pantalla.add(new Perfil());
 		pantalla.add(new Recursos("images/diedral01.png"));
-		pantalla.add(new Sesiones("images/diedralcolor.png"));
-		pantalla.add(new Usuarios("images/diedral5.png"));
+		pantalla.add(new Sesiones());
+		pantalla.add(new Usuarios());
 		pantalla.add(new Actividades("images/diedral22.png"));
 		
 		pantalla.setSize(1200, 800);
@@ -27,11 +26,11 @@ public class Main {
 		JFrame pantalla = new JFrame();
 		pantalla.setLayout(new GridLayout(3, 3, 1, 1));
 		pantalla.add(new Calendario("images/diedral00.png"));
-		pantalla.add(new Pacientes("images/diedral00.png"));
-		pantalla.add(new Perfil("images/diedral00.png"));
+		pantalla.add(new Pacientes());
+		pantalla.add(new Perfil());
 		pantalla.add(new Recursos("images/diedral01.png"));
-		pantalla.add(new Sesiones("images/diedral5.png"));
-		pantalla.add(new Usuarios("images/diedral5.png"));
+		pantalla.add(new Sesiones());
+		pantalla.add(new Usuarios());
 		pantalla.add(new Actividades("images/diedral22.png"));
 		pantalla.setSize(1200, 800);
 		pantalla.setVisible(true);
