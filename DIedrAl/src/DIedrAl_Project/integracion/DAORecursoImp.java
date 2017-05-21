66fedfcb41e1311613e4fe90a5f21ddc1fff0c44
Galieve <enriqueroman@live.com>
2017-05-21 17:50:09 +0200

    Add files via upload

--- DIedrAl/src/DIedrAl_Project/integracion/DAORecursoImp.java
+++ DIedrAl/src/DIedrAl_Project/integracion/DAORecursoImp.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import DIedrAl_Project.integracion.BasicClasses.DAObasico;
-import DIedrAl_Project.negocio.administracion.Terapeuta;
 import DIedrAl_Project.negocio.recursos.Recurso;
 
 public class DAORecursoImp implements DAORecurso{
