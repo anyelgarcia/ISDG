114de02a29895e502ae8d697b592dcde28436cd5
Cere <aleher06@ucm.es>
2017-05-31 17:59:27 +0200

    Objetos Serializables ya serializados + DAOCalendario

--- DIedrAl/src/DIedrAl_Project/negocio/pacientes/Direccion.java
+++ DIedrAl/src/DIedrAl_Project/negocio/pacientes/Direccion.java
@@ -1,6 +1,8 @@
 package DIedrAl_Project.negocio.pacientes;
 
-public class Direccion {
+import java.io.Serializable;
+
+public class Direccion implements Serializable {
 	private String localidad;
 
 	private String provincia;
