6a50d4ca359d378f77a58f6f2b025bb763d19ef3
Galieve <enriqueroman@live.com>
2017-06-01 19:46:27 +0200

    Ligar, parte 1

--- DIedrAl/src/DIedrAl_Project/negocio/servicioDeAplicaciones/SACalendarioImpl.java
+++ DIedrAl/src/DIedrAl_Project/negocio/servicioDeAplicaciones/SACalendarioImpl.java
@@ -1,14 +1,50 @@
 package DIedrAl_Project.negocio.servicioDeAplicaciones;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 
 import DIedrAl_Project.integracion.BasicClasses.AccessException;
+import DIedrAl_Project.integracion.DAOinterfaces.DAOActividad;
+import DIedrAl_Project.integracion.DAOinterfaces.DAORecurso;
+import DIedrAl_Project.integracion.DAOinterfaces.DAOSesion;
+import DIedrAl_Project.integracion.DAOinterfaces.DAOSesionProgramada;
+import DIedrAl_Project.integracion.simplefileImp.SimpleFileDAOFactory;
 import DIedrAl_Project.negocio.calendario.*;
+import DIedrAl_Project.negocio.recursos.Actividad;
 import DIedrAl_Project.negocio.recursos.ArraySesionesProgramadas;
+import DIedrAl_Project.negocio.recursos.Banco;
+import DIedrAl_Project.negocio.recursos.Recurso;
+import DIedrAl_Project.negocio.recursos.Sesion;
 
 public class SACalendarioImpl implements SACalendario {
+	
 
 
+	private static SACalendarioImpl instancia;
+	private DAORecurso daorec;
+	private DAOActividad daoact;
+	private DAOSesion daoses;
+	private DAOSesionProgramada daosp;
+	private Banco bank;
+	
+	public static SACalendario getInstancia() {
+
+		if (instancia == null) {
+			instancia = new SACalendarioImpl();
+		}
+
+		return instancia;
+	}
+
+	private SACalendarioImpl() {
+		SimpleFileDAOFactory factoria = SimpleFileDAOFactory.getInstance();
+		daorec = factoria.getDAORecurso();
+		daoact = factoria.getDAOActividad();
+		daoses = factoria.getDAOSesion();
+		daosp = factoria.getDAOSesionProgramada();
+		bank = Banco.getInstancia();
+		cargarBanco();
+	}
 	@Override
 	public void addSesion(SesionProgramada sesion) {
 		mes.addSesion(sesion);
@@ -45,4 +81,41 @@ public class SACalendarioImpl implements SACalendario {
 		return null;
 	}
 
+	
+	private void cargarBanco() {
+		try {
+			HashSet<Actividad> set = daoact.listarActividades();
+			for (Actividad a : set) {
+				bank.addActividad(a);
+			}
+		} catch (AccessException e) {
+			// e.printStackTrace();
+		}
+		try {
+			HashSet<Sesion> ses = daoses.listarSesiones();
+			for (Sesion s : ses) {
+				bank.addSesion(s);
+			}
+		} catch (AccessException e) {
+			// e.printStackTrace();
+		}
+
+		try {
+			HashSet<Recurso> rec = daorec.listarRecursos();
+			for (Recurso r : rec) {
+				bank.addRecurso(r);
+			}
+		} catch (AccessException e) {
+			// e.printStackTrace();
+		}
+		
+		try {
+			HashSet<SesionProgramada> sep = daosp.listarSesionesProgramadas();
+			for (SesionProgramada s : sep) {
+				bank.addSesionProgramada(s);
+			}
+		} catch (AccessException e) {
+			// e.printStackTrace();
+		}
+	}
 }
