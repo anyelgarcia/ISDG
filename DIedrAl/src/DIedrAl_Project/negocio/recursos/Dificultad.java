143990c761d397ddcc0235b628507cc4c46a29b6
anyelpolla <angel.garcia11@um.es>
2017-06-02 14:19:12 +0200

    hulsqgdfuoqesgufd

--- DIedrAl/src/DIedrAl_Project/negocio/recursos/Dificultad.java
+++ DIedrAl/src/DIedrAl_Project/negocio/recursos/Dificultad.java
@@ -5,5 +5,16 @@ import java.io.Serializable;
 
 public enum Dificultad implements Serializable{
 	
-	MUY_FACIL, FACIL, MEDIO, DIFICIL, MUY_DIFICIL,
+	MUY_FACIL, FACIL, MEDIO, DIFICIL, MUY_DIFICIL;
+	
+	public static Dificultad toDificultad(String a){
+		switch(a){
+			case "MUY_FACIL": return Dificultad.MUY_FACIL; 
+			case "FACIL": return Dificultad.FACIL; 
+			case "MEDIO": return Dificultad.MEDIO; 
+			case "DIFICIL": return Dificultad.DIFICIL;
+			case "MUY_DIFICIL": return Dificultad.MUY_DIFICIL;
+			default: return null;
+		}
+	}
 }
